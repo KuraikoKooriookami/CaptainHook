d919a96ea2dcb01a792e62a154fd3f676dae1d0e
Kuraiko <marvin.stiels@stud.hn.de>
2025-01-07 20:14:03 +0100

    improved Collision

--- CaptainHook.cpp
+++ CaptainHook.cpp
@@ -13,7 +13,7 @@ struct WorldState {
 	SDL_Renderer* renderer;
 	SDL_Surface* image;
 	SDL_Texture* texture;
-	SDL_FPoint playerPosition = { 100, 100 };
+	SDL_Rect player = { 100, 100, 50, 50 };
 	SDL_Rect camera = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
 	double speedX = 0;
 	double speedY = 0;
@@ -80,6 +80,7 @@ void readEvents(WorldState& ws) {
 		ws.speedY += v.GRAVITY;
 	}
 	else {
+		ws.speedY = 0;
 		ws.jumps = 0;
 		if (ws.speedX > v.GROUND_FRICTION) {
 			ws.speedX += v.GROUND_FRICTION * -1;
@@ -95,16 +96,45 @@ void readEvents(WorldState& ws) {
 	}
 }
 bool checkCollision(const WorldState& ws, const SDL_FRect& b) {
-	SDL_Rect a = { ws.playerPosition.x, ws.playerPosition.y, 50, 50 };
+	SDL_Rect a = ws.player;
 	return (a.x < b.x + b.w &&
 		a.x + a.w > b.x &&
 		a.y < b.y + b.h &&
 		a.y + a.h > b.y);
 }
 
+void resolveCollision(WorldState& ws, const SDL_FRect& obstacle) {
+
+	// Determine the overlap in each direction
+	float overlapLeft = (ws.player.x + ws.player.w) - obstacle.x;
+	float overlapRight = (obstacle.x + obstacle.w) - ws.player.x;
+	float overlapTop = (ws.player.y + ws.player.h) - obstacle.y;
+	float overlapBottom = (obstacle.y + obstacle.h) - ws.player.y;
+
+	// Find the minimum overlap to resolve collision
+	float minOverlap = std::min({ overlapLeft, overlapRight, overlapTop, overlapBottom });
+
+	if (minOverlap == overlapLeft) {
+		// Colliding from the left
+		ws.player.x = obstacle.x - ws.player.w;
+	}
+	else if (minOverlap == overlapRight) {
+		// Colliding from the right
+		ws.player.x = obstacle.x + obstacle.w;
+	}
+	else if (minOverlap == overlapTop) {
+		// Colliding from the top
+		ws.player.y = obstacle.y - ws.player.h;
+	}
+	else if (minOverlap == overlapBottom) {
+		// Colliding from the bottom
+		ws.player.y = obstacle.y + obstacle.h;
+	}
+}
+
 void handle_camera(WorldState& ws) {
-	ws.camera.x = (ws.playerPosition.x + 50 / 2) - SCREEN_WIDTH  /  2;
-	ws.camera.y = (ws.playerPosition.y + 50 / 2) - SCREEN_HEIGHT / 2;
+	ws.camera.x = (ws.player.x + 50 / 2) - SCREEN_WIDTH  /  2;
+	ws.camera.y = (ws.player.y + 50 / 2) - SCREEN_HEIGHT / 2;
 	
 	if (ws.camera.x < 0)
 	{
@@ -117,25 +147,36 @@ void handle_camera(WorldState& ws) {
 }
 
 void mutateWorldState(WorldState& ws, const vector<SDL_FRect>& obstacles) {
-	ws.speedX += ws.movement;
-	ws.playerPosition.x += ws.speedX;
-	ws.speedX -= ws.movement;
-	ws.playerPosition.y += ws.speedY;
 
 	ws.ground = false;
+	ws.player.y += ws.speedY;
 	for (const auto& obstacle : obstacles) {
 		if (checkCollision(ws, obstacle)) {
 			if (ws.speedY > 0) {
-				ws.playerPosition.y = obstacle.y - 50; // Adjust player position to be on top of the obstacle
+				ws.player.y = obstacle.y - ws.player.h; // Adjust player position to be on top of the obstacle
 				ws.ground = true;
 				ws.speedY = 0;
 			}
 			else {
-				ws.playerPosition.y = obstacle.y + 50; // Adjust player position to be on top of the obstacle
+				ws.player.y = obstacle.y + obstacle.h; // Adjust player position to be on top of the obstacle
 				ws.ground = false;
 				ws.speedY = 0;
 			}
-			break;
+		}
+	}
+	ws.speedX += ws.movement;
+	ws.player.x += ws.speedX;
+	ws.speedX -= ws.movement;
+	for (const auto& obstacle : obstacles) {
+		if (checkCollision(ws, obstacle)) {
+			if (ws.speedX > 0) {
+				ws.player.x = obstacle.x - ws.player.w; // Adjust player position to be on top of the obstacle
+				ws.speedX = 0;
+			}
+			else {
+				ws.player.x = obstacle.x + obstacle.w; // Adjust player position to be on top of the obstacle
+				ws.speedX = 0;
+			}
 		}
 	}
 }
@@ -152,7 +193,7 @@ void renderGraphics(WorldState& ws, const vector<SDL_FRect>& obstacles) {
 	}
 
 	SDL_SetRenderDrawColor(ws.renderer, 0, 0, 255, 255);
-	SDL_Rect playerRect = { ws.playerPosition.x - ws.camera.x, ws.playerPosition.y - ws.camera.y, 50, 50 };
+	SDL_Rect playerRect = { ws.player.x - ws.camera.x, ws.player.y - ws.camera.y, 50, 50 };
 	SDL_RenderFillRect(ws.renderer, &playerRect);
 	SDL_RenderPresent(ws.renderer);
 	
@@ -163,9 +204,10 @@ int main(int argc, char* args[])
 	WorldState worldState;
 	initWorldState(worldState);
 	vector<SDL_FRect> obstacles = {
-		{0, 550, 850, 50}, // Example ground object
+		{0, 550, 1050, 50}, // Example ground object
 		{200, 400, 100, 50}, // Example obstacle
-		{400, 300, 150, 50}  // Another example obstacle
+		{400, 300, 350, 50},  // Another example obstacle
+		{850, 200, 50, 300}  // Wall example obstacle
 	};
 
 	while (true) {
