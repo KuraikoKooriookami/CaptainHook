99dd563ff3ce63d12ec80b288350caa5efd3e608
KuraikoKooriookami <Kuraiko.stiels@gmail.com>
2025-01-12 00:56:00 +0100

    added wild magic

--- CaptainHook.cpp
+++ CaptainHook.cpp
@@ -297,64 +297,6 @@ void readEvents(WorldState& ws, const vector<SDL_FRect>& obstacles) {
 		}
 	}
 }
-/*func _physics_process(_delta: float) -> void:
-# Walking
-var walk = (Input.get_action_strength("right") - Input.get_action_strength("left")) * MOVE_SPEED
-
-# Falling
-velocity.y += GRAVITY
-
-# Hook physics
-if $Chain.hooked:
-# `to_local($Chain.tip).normalized()` is the direction that the chain is pulling
-chain_velocity = to_local($Chain.tip).normalized() * CHAIN_PULL
-if chain_velocity.y > 0:
-# Pulling down isn't as strong
-chain_velocity.y *= 0.55
-else:
-# Pulling up is stronger
-chain_velocity.y *= 1.65
-if sign(chain_velocity.x) != sign(walk) :
-# if we are trying to walk in a different
-	# direction than the chain is pulling
-	# reduce its pull
-	chain_velocity.x *= 0.7
-else:
-# Not hooked->no chain velocity
-chain_velocity = Vector2(0, 0)
-velocity += chain_velocity
-
-velocity.x += walk		# apply the walking
-move_and_slide(velocity, Vector2.UP)	# Actually apply all the forces
-velocity.x -= walk		# take away the walk speed again
-# ^ This is done so we don't build up walk speed over time
-
-# Manage friction and refresh jump and stuff
-velocity.y = clamp(velocity.y, -MAX_SPEED, MAX_SPEED)	# Make sure we are in our limits
-velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
-var grounded = is_on_floor()
-if grounded:
-velocity.x *= FRICTION_GROUND	# Apply friction only on x(we are not moving on y anyway)
-can_jump = true 				# We refresh our air - jump
-if velocity.y >= 5:		# Keep the y - velocity small such that
-velocity.y = 5		# gravity doesn't make this number huge
-elif is_on_ceiling() and velocity.y <= -5:	# Same on ceilings
-velocity.y = -5
-
-# Apply air friction
-if !grounded:
-velocity.x *= FRICTION_AIR
-if velocity.y > 0:
-velocity.y *= FRICTION_AIR
-
-# Jumping
-if Input.is_action_just_pressed("jump") :
-	if grounded :
-		velocity.y = -JUMP_FORCE	# Apply the jump - force
-		elif can_jump :
-can_jump = false	# Used air - jump
-velocity.y = -JUMP_FORCE
-*/
 
 void handle_camera(WorldState& ws) {
 	camera.x = (ws.player.x + ws.player.w / 2) - SCREEN_WIDTH  /  2;
@@ -406,21 +348,11 @@ void mutateWorldState(WorldState& ws, const vector<SDL_FRect>& obstacles) {
 				ws.hookNotConnected = false;
 			}
 		}
-	}
-	if (ws.hookConnected) {
-		hookPull = calculateDirection(ws.player, ws.hookGoal);
-		if (hookPull.y < 0)
-			speedVector.y += hookPull.y * ws.HOOKSTRENGHT * 1.5;
-		else
-			speedVector.y += hookPull.y * ws.HOOKSTRENGHT * 0.85;
-		if (hookPull.x < 0 && ws.movement < 0)
-			speedVector.x += hookPull.x * ws.HOOKSTRENGHT * 0.9;
-		else
-			speedVector.x += hookPull.x * ws.HOOKSTRENGHT;
-	}
-
+	}	
+	
 	if (ws.ground == false) {
-		ws.appliedForce.y += v.GRAVITY;
+		if(!ws.hookConnected)
+			ws.appliedForce.y += v.GRAVITY;
 		ws.appliedForce.x *= v.AIR_FRICTION;
 	}
 	else {
@@ -432,11 +364,44 @@ void mutateWorldState(WorldState& ws, const vector<SDL_FRect>& obstacles) {
 			ws.playerVelocity.x += v.GROUND_FRICTION;
 		}
 	}
+
+	if (ws.hookConnected) {
+		hookPull = calculateDirection(ws.player, ws.hookGoal);
+		if (hookPull.y < 0)
+			speedVector.y += hookPull.y * ws.HOOKSTRENGHT * 1.5;
+		else
+			speedVector.y += hookPull.y * ws.HOOKSTRENGHT * 0.85;
+		if (hookPull.x < 0 && ws.movement < 0)
+			speedVector.x += hookPull.x * ws.HOOKSTRENGHT * 0.9;
+		else
+			speedVector.x += hookPull.x * ws.HOOKSTRENGHT;
+
+		if (speedVector.x > 0 && speedVector.x > ws.appliedForce.x) {
+			ws.appliedForce.x += speedVector.x;
+		}		
+		if (speedVector.x < 0 && speedVector.x < ws.appliedForce.x) {
+			ws.appliedForce.x += speedVector.x;
+		}
+		if (speedVector.y > 0 && speedVector.y > ws.appliedForce.y) {
+			ws.appliedForce.y += speedVector.y;
+			if (ws.appliedForce.y > 0)
+				ws.appliedForce.y = 0;
+		}
+		if (speedVector.y < 0 && speedVector.y < ws.appliedForce.y) {
+			ws.appliedForce.y += speedVector.y;
+			if (ws.appliedForce.y < 0)
+				ws.appliedForce.y = 0;
+		}
+	}
+
 	ws.ground = false;
 
 	ws.playerVelocity += ws.appliedForce;
 	ws.playerVelocity.x = SDL_clamp(ws.playerVelocity.x, -v.MAXSPEED, v.MAXSPEED);
 	ws.playerVelocity.y = SDL_clamp(ws.playerVelocity.y, -v.MAXSPEED, v.MAXSPEED);
+	if (ws.playerVelocity.x < 1 && ws.playerVelocity.x > -1) {
+		ws.playerVelocity.x = 0;
+	}
 	ws.player.y += ws.playerVelocity.y + speedVector.y;
 
 	for (const auto& obstacle : obstacles) {
@@ -467,10 +432,6 @@ void mutateWorldState(WorldState& ws, const vector<SDL_FRect>& obstacles) {
 				ws.playerVelocity.x = 0;
 			}
 		}
-	}
-
-	if (ws.playerVelocity.x < 1 && ws.playerVelocity.x > -1) {
-		ws.playerVelocity.x = 0;
 	}
 }
 
